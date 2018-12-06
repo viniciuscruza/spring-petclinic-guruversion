7fb1d102da0e63b7f4620eff76ef40dc7329970e
Vinicius Alves <vinicius.cruza@gmail.com>
2018-12-05 22:01:11 -0200

    Initial commit

---
+++ src/main/java/vinicius/petclinic/demo/MyversionApplication.java
@@ -0,0 +1,12 @@
+package vinicius.petclinic.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyversionApplication.class, args);
+    }
+}
